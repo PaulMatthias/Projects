851596f2a2525551fc75c120ec478df042d48431
PaulMatthias <pm101481@frontend.service>
2017-02-17 14:23:00 +0100

    update PlayerAI

--- AI/2048/PlayerAI.py
+++ AI/2048/PlayerAI.py
@@ -30,6 +30,10 @@ class PlayerAI(BaseAI):
         time_calc=timeit.default_timer()
         fringe=[]
         fringe.append(grid)
+	alpha=-infinity
+	beta=+infinity
+	depth=8
+	
         while time_calc-time<0.09:
             max_list=[]
             fringe_new=[]
@@ -37,14 +41,11 @@ class PlayerAI(BaseAI):
 
             #explore the whole fringe and move the tiles (Players Turn)
             for el in xrange(0,len(fringe)):
-                moves=fringe[el].getAvailableMoves()
-                for move in moves:
-                    grid_exp=copy.deepcopy(fringe[el])
-                    grid_exp.move(move)
-                    max_list.append(heur_order(grid_exp))
-                    moves_done.append(move)
-                    fringe_new.append(grid_exp)
-                fringe.pop(0)
+	      max(fringe[el], depth, alpha, beta)
+
+            #explore the whole finge and spawn a new tile (Computers Turn)
+            for el in xrange(0,len(fringe)):
+	      min(fringe[el], depth, alpha, beta)
 
             index_max=max_list.index(max(max_list))
             fringe.append(fringe_new[index_max])
@@ -68,30 +69,71 @@ class PlayerAI(BaseAI):
 #
 #            index_max=numpy.argmax(max_list)
 #            fringe.append(fringe_new[index_max])
-
-
-
-
-
-
-
-            
-
-
-            
-
-
-
-
-            
-
-
-
-
-
-
-
-
-
-
         return self.turn
+
+#int max(int tiefe,
+#int alpha, int beta) {
+#if (tiefe == 0 or keineZuegeMehr(spieler_max))
+#return bewerten();
+#int maxWert = alpha;
+#generiereMoeglicheZuege(spieler_max);
+#while (noch Zug da) {
+#fuehreNaechstenZugAus();
+#int wert = min(tiefe-1, 
+#maxWert, beta);
+#macheZugRueckgaengig();
+#if (wert > maxWert) {
+# maxWert = wert;
+#if (maxWert >= beta)
+#break;
+#if (tiefe == anfangstiefe)
+#       gespeicherterZug = Zug;
+#	      }
+#}
+#return maxWert;
+#}
+
+def max(grid, depth, alpha, beta):
+  if(depth==0):
+    return move
+  maxVal=alpha
+
+  moves=grid.getAvailableMoves()
+  for move in moves:
+      grid_exp=copy.deepcopy(grid)
+      grid_exp.move(move)
+      beta=heur_order(grid_exp)
+      #max_list.append(heur_order(grid_exp))
+      #moves_done.append(move)
+      #fringe_new.append(grid_exp)
+      val=min(depth-1,maxVal,beta)
+      if(val>maxVal):
+	maxVal=val
+	if(maxVal>beta):
+	  break
+	if(depth==start_depth):
+	  saved_move=move
+  return maxVal
+
+
+def min(grid, depth, alpha, beta):
+  if(depth==0):
+    return move
+  minVal=beta
+
+  moves=grid.getAvailableMoves()
+  for move in moves:
+      grid_exp=copy.deepcopy(grid)
+      grid_exp.move(move)
+      beta=heur_order(grid_exp)
+      #max_list.append(heur_order(grid_exp))
+      #moves_done.append(move)
+      #fringe_new.append(grid_exp)
+      val=min(depth-1,alpha,minVal)
+      if(val<minVal):
+	minVal=val
+	if(minVal<alpha):
+	  break
+	if(depth==start_depth):
+	  saved_move=move
+  return minVal
